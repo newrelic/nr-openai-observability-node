8dd356ccaceb463c405b1a7da9ce86b9b47f774e
Vadim Siomin <vsiomin@newrelic.com>
2023-06-07 18:29:39 +0300

    remove dist

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.monitorOpenAI = void 0;
-var monitor_1 = require("./monitor");
-Object.defineProperty(exports, "monitorOpenAI", { enumerable: true, get: function () { return monitor_1.monitorOpenAI; } });
-//# sourceMappingURL=index.js.map
